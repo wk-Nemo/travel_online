001f7ef4edd1c1928ed0545512da183929dbf810
wk-Nemo <2181544173@qq.com>
2021-03-12 21:16:09 +0800

    port change

--- index.js
+++ index.js
@@ -4016,4 +4016,4 @@ app.get('/detail.json',(req,res)=>{
       )
 })
 
-app.listen(80, () => console.log('**********【服务器启动成功】**********'));
+app.listen(5000, () => console.log('**********【服务器启动成功】**********'));
